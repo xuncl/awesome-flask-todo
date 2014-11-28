8e9c53473e12c31c5135ddde2f8d9ff8267ec019
defshine <crazyxin1988@163.com>
2014-11-28 23:09:31 +0800

    v0.5 save todo

--- app/views.py
+++ app/views.py
@@ -1,8 +1,17 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 from models import Todo
 
 @app.route('/')
 def index():
+    todos = Todo.objects.all()
+    return render_template("index.html", todos=todos)
+
+
+@app.route('/add', methods=['POST', ])
+def add():
+    content = request.form.get("content")
+    todo = Todo(content=content)
+    todo.save()
     todos = Todo.objects.all()
     return render_template("index.html", todos=todos)
