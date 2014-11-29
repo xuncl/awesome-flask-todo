3df214a660b3be06248563179c53ececfd0c071d
defshine <crazyxin1988@163.com>
2014-11-29 22:25:31 +0800

    v0.8 validate

--- app/views.py
+++ app/views.py
@@ -1,38 +1,43 @@
 from app import app
 from flask import render_template, request
-from models import Todo
+from models import Todo, TodoForm
 
 @app.route('/')
 def index():
+    form = TodoForm()
     todos = Todo.objects.all()
-    return render_template("index.html", todos=todos)
+    return render_template("index.html", todos=todos, form=form)
 
 
 @app.route('/add', methods=['POST', ])
 def add():
-    content = request.form.get("content")
-    todo = Todo(content=content)
-    todo.save()
+    form = TodoForm(request.form)
+    if form.validate():
+        content = form.content.data
+        todo = Todo(content=content)
+        todo.save()
     todos = Todo.objects.all()
-    return render_template("index.html", todos=todos)
+    return render_template("index.html", todos=todos, form=form)
 
 
 @app.route('/done/<string:todo_id>')
 def done(todo_id):
+    form = TodoForm()
     todo = Todo.objects.get_or_404(id=todo_id)
     todo.status = 1
     todo.save()
     todos = Todo.objects.all()
-    return render_template("index.html", todos=todos)
+    return render_template("index.html", todos=todos, form=form)
 
 
 @app.route('/undone/<string:todo_id>')
 def undone(todo_id):
+    form = TodoForm()
     todo = Todo.objects.get_or_404(id=todo_id)
     todo.status = 0
     todo.save()
     todos = Todo.objects.all()
-    return render_template("index.html", todos=todos)
+    return render_template("index.html", todos=todos, form=form)
 
 
 @app.route('/delete/<string:todo_id>')
@@ -40,4 +45,4 @@ def delete(todo_id):
     todo = Todo.objects.get_or_404(id=todo_id)
     todo.delete()
     todos = Todo.objects.all()
-    return render_template("index.html", todos=todos)
+    return render_template("index.html", todos=todos, form=form)
